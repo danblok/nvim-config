d57e3472f31477860cc5ec9b1839aa1be7fa4ed8
danblok <danytokmakov@gmail.com>
2024-03-30 12:12:04 +0500

    change color of bool and number to blue

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,8 +7,8 @@ return {
             vim.cmd.colorscheme("gruvbox-material")
             vim.api.nvim_set_hl(0, "GreenBold", { fg = "#d3869b" })
             vim.api.nvim_set_hl(0, "@variable.member", { fg = "#d4be98" })
-            vim.api.nvim_set_hl(0, "@boolean", { fg = "#ea6962" })
-            vim.api.nvim_set_hl(0, "@number", { fg = "Fg" })
+            vim.api.nvim_set_hl(0, "@boolean", { fg = "#7daea3" })
+            vim.api.nvim_set_hl(0, "@number", { fg = "#7daea3" })
             vim.api.nvim_set_hl(0, "@string.escape", { fg = "#7daea3" })
             vim.api.nvim_set_hl(0, "Normal", { bg = "#292828" })
         end,
