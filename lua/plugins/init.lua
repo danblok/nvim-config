7cc6cdf261394c9aba5a54ed6e1d13e699837804
danblok <danytokmakov@gmail.com>
2024-09-02 12:05:06 +0500

    Add vim-abolish and Undotree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,4 +26,15 @@ return {
             require("mini.surround").setup(config)
         end,
     },
+    {
+        "tpope/vim-abolish",
+        config = function() end,
+    },
+    {
+        "mbbill/undotree",
+        keys = {
+            { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "Toggle Undotree" },
+        },
+        config = function() end,
+    },
 }
