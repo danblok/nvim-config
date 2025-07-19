5e6ab109099c3e0e14aaeba8edf789a1c335bfaa
danblok <danytokmakov@gmail.com>
2025-07-19 12:33:25 +0500

    Switch to catppuccin and fix the telescope-fzf-native build error

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,20 @@
 return {
     {
-        "rmehri01/onenord.nvim",
-        lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         config = function()
-            require("onenord").setup()
-            vim.cmd.colorscheme("onenord")
-        end,
+            require("catppuccin").setup()
+            vim.cmd.colorscheme("catppuccin-frappe")
+        end
     },
+    -- {
+    --     "rmehri01/onenord.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("onenord").setup()
+    -- vim.cmd.colorscheme("onenord")
+    --     end,
+    -- },
 }
